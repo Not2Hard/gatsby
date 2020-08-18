289f846a88832cc1c18ae93a08ca993bf7e50137
Natalia Sergeeva <natalia@uxcollection.com>
2020-08-18 16:59:19 -0400

    gatsby-conftg

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built 888 99with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
