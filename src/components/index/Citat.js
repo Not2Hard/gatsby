bd511efd55030f27c010d36f05d154ca0a800061
Natalia Sergeeva <natalia@uxcollection.com>
2020-08-20 16:26:47 -0400

    1st page done

--- src/components/index/Citat.js
+++ src/components/index/Citat.js
@@ -1,8 +1,33 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
+
+import citatimg from '../../images/tango_citat.svg'
+import { CitatWrapper } from './styles/CitatStyles'
 
 const Citat = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            wordpressPage(wordpress_id: { eq: 47}) {
+                acf {
+                citat_1_text
+                citat_1_author
+                }
+            }
+        }
+    `)
+
     return ( 
-    <div>Citat</div> 
+         <CitatWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img src={citatimg} alt="quote" />
+            <h6>{data.wordpressPage.acf.citat_1_text}</h6>
+            {data.wordpressPage.acf.citat_1_author}
+          </div>
+        </div>
+      </div>
+    </CitatWrapper>
     );
 }
  
